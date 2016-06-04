816e1e61b1c5250747d8aae533d8e31dffc6e4ef
crazyBaboon <nuno360@gmail.com>
2016-06-04 20:38:01 +0100

    Added support for inelastic collisions
    Added support for inelastic collisions between bodies. All the changes are in file simulation.py. Added authors file. Included file equivalent to main.py to illustrate hypothetical collisions in the solar system.

--- simulation.py
+++ simulation.py
@@ -1,3 +1,6 @@
+import math
+AU = 149.6e9
+
 class Simulation(object):
 
     def __init__(self):
@@ -14,9 +17,20 @@ class Simulation(object):
             for other in self.bodies:
                 if other is body:
                     continue
-                body.attraction(other)
-                total_fx += body.fx
-                total_fy += body.fy
+                #Check if there are collisions and calculate velocity, mass and radius thereafter (inelastic collisions)
+                elif (math.sqrt((body.x-other.x)**2 + (body.y-other.y)**2)/AU<0.1 ):
+                    body.vx= (body.mass*body.vx + other.mass*other.vx)/(body.mass+other.mass)
+                    body.vy= (body.mass*body.vy + other.mass*other.vy)/(body.mass+other.mass)
+                    body.mass=body.mass+other.mass
+                    body.radius=body.radius+other.radius
+                    self.bodies.remove(other)
+                else:
+                    body.attraction(other)
+                    total_fx += body.fx
+                    total_fy += body.fy
+
+
+
 
             body.fx = total_fx
             body.fy = total_fy
---
+++ solar_system_collisions.py
@@ -0,0 +1,85 @@
+import math
+
+from body import Body
+from simulation import Simulation
+from renderer import Renderer
+
+##################
+# Simulation setup
+AU = (149.6e6 * 1000)  # 149.6 million km, in meters.
+timestep = 24*3600
+
+sim = Simulation()
+
+sun = Body('Sun')
+sun.mass = 1.98892e30
+sim.add_body(sun)
+
+mercury = Body('Mercury')
+mercury.mass = 3.3e23
+mercury.x = 0.387 * AU
+mercury.vy = 47362
+sim.add_body(mercury)
+
+venus = Body('Venus')
+venus.mass = 4.8685e24
+venus.x = 0.723 * AU
+venus.vy = 35020
+sim.add_body(venus)
+
+earth = Body('Earth')
+earth.mass = 5.9742e29
+earth.x = AU
+earth.vy = 0.1
+sim.add_body(earth)
+
+mars = Body('Mars')
+mars.mass = 6.42e23
+mars.x = 1.52 * AU
+mars.vy = 24077
+sim.add_body(mars)
+
+#################
+# Rendering setup
+interface = Renderer(sim, AU, 800, 600)
+
+sun.material['emission'] = [1.0, 0.7, 0.0, 1.0]
+sun.radius = 0.05
+
+mercury.material['diffuse'] = [0.65, 0.65, 0.65, 1.0]
+mercury.material['ambient'] = [0.8, 0.8, 0.8, 1.0]
+mercury.radius = 0.01915
+
+venus.material['diffuse'] = [0.72, 0.64, 0.44, 1.0]
+venus.material['ambient'] = [0.72, 0.64, 0.44, 1.0]
+venus.radius = 0.0475
+
+earth.material['diffuse'] = [70/255, 120/255, 200/255, 1.0]
+earth.material['ambient'] = [70/255, 120/255, 200/255, 1.0]
+earth.radius = 0.05
+
+mars.material['diffuse'] = [216/255, 109/255, 47/255, 1.0]
+mars.material['ambient'] = [216/255, 109/255, 47/255, 1.0]
+mars.radius = 0.0266
+
+
+
+##############
+# Program loop
+while True:
+    sim.tick(timestep)
+    print('#---------#')
+    for body in sim.bodies:
+
+        print('{}\tx: {:7.3f}  y: {:7.3f}  vx: {:10.3f}  vy: {:10.3f}'
+              .format(body.name, body.x / AU, body.y / AU, body.vx, body.vy))
+        print('\tvelocity: {:8.3f} km/s  distance from sun: {:5.3f} AU'
+              .format(math.sqrt(body.vx**2 + body.vy**2),
+                      math.sqrt(body.x**2 + body.y**2) / AU))
+
+
+
+
+
+        # render stuff here
+        interface.render()
